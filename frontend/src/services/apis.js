ef19f86b5d8685296d4e2db91ce1a18e5524f0a2
Resull Gencerr <resullgencerr@gmail.com>
2025-05-22 05:51:40 +0300

    API base URL güncellendi2

--- frontend/src/services/apis.js
+++ frontend/src/services/apis.js
@@ -1,5 +1,5 @@
 //const BASE_URL = "http://localhost:5000/api/v1";
-const BASE_URL = "https://studyplatform-szz6.onrender.com";
+const BASE_URL = "https://studyplatform-szz6.onrender.com/api/v1";
 
 // AUTH ENDPOINTS
 export const endpoints = {
